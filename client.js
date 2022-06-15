82bc5d45a7b8741a10b587242ce7ab39906201d0
Diego Baeza <diegob12@live.com>
2022-06-15 09:50:09 -0500

    attempt many variations but no functionality was accomplished

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const employees = [
+let employees = [
   {
     name: 'Atticus',
     employeeNumber: '2405',
@@ -32,11 +32,46 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-
 // This problem is massive! Break the problem down, take small steps, and test as you go.
 // What is the fewest lines of code I can write and test to get just a little closer?
-
+let newEmployeeGraphic = [], newCompensation = {}; let object= {}
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+// for (let i=0; i<array.length; i++){
+function gatherNames(array){
+  for (let i=0; i<array.length; i++) 
+  newEmployeeGraphic.push(array[i].name)
+return newEmployeeGraphic;
+}
 
-console.log( employees );
+function percentageInputs(array){
+  let bonusPercentage
+  for (let i=0; i<array.length; i++)
+   if (array.reviewRating <= 2) {bonusPercentageInput = 'No bonus percentage'};
+   if (object.reviewRating === 3) {bonusPercentageInput = '4%'}
+   if (object.reviewRating === 4) {bonusPercentageInput = '6%'}
+   if (object.reviewRating === 5) {bonusPercentageInput = '10%'}
+}
+function percentageInputs(array) {
+  for (let i=0; i<array.length; i++) 
+  newEmployeeGraphic.push(array[i].name)
+return newEmployeeGraphic;
+}
+
+// function calculations(array) {
+//   let newCompensation = {
+//     name:
+//     // bonusPercentage: bonusPercentageInput,
+//     // totalCompensation: totalCompensation,
+//     // totalBonus: totalBonusInput
+//   }
+//   for (let i=0; i<array.length; i++){
+//     if (object.reviewRating <= 2) {bonusPercentageInput = 'No bonus percentage'} {
+//     newCompensation.push(array[i].nameInput)
+//   }
+// }
+// }
+
+
+
+console.log(gatherNames(employees));
